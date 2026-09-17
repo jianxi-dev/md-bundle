10c36626577c2ca28d82f53bdcbfdf83af34cf19
lqtdys <tao0101@163.com>
2026-09-18 07:36:48 +0800

    feat(editor): structure linter with AST-based diagnosis rules (refs #139)

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -28,28 +28,38 @@ export type { ImageResolver, ImageCallbacks } from './decorations/image';
 export { getBlocks, getBlockAt, type Block, type BlockType } from './block-model';
 export { CommandRegistry, type Command } from './commands';
 export {
-  contextToolbar,
-  updateToolbar,
-  hideContextToolbar,
-  detectContext,
-  getButtonsForContext,
-  type ToolbarContext,
-  type ToolbarButton,
-} from './toolbar';
+  resolveProvider,
+  createLocalProvider,
+  createBYOKeyProvider,
+  createAIProposal,
+  applyProposal,
+  rejectProposal,
+  createAIDiffViewPlugin,
+  aiDiffAcceptHunk,
+  aiDiffRejectHunk,
+  aiDiffAcceptAll,
+  createPrivacyLedger,
+  type AIProvider,
+  type AIProviderConfig,
+  type AIResolution,
+  type AICapability,
+  type StreamParams,
+  type AIProposal,
+  type AIDiffViewPlugin,
+  type AIDiffState,
+  type PrivacyLedger,
+  type PrivacyLedgerEntry,
+} from './ai';
 export {
-  smartInput,
-  handleMarkdownShortcut,
-  smartEnter,
-  smartBackspace,
-  handleAutoPair,
-  handleChinesePair,
-} from './smart-input';
-export {
-  contextToolbar, updateToolbar, hideContextToolbar,
-  detectContext, getButtonsForContext,
-  type ToolbarContext, type ToolbarButton,
-} from './toolbar';
-export {
-  smartInput, handleMarkdownShortcut, smartEnter,
-  smartBackspace, handleAutoPair, handleChinesePair,
-} from './smart-input';
+  getHeadings,
+  buildTree,
+  computeSections,
+  moveSection,
+  extractSubtree,
+  flattenTree,
+  chapterReorgExtension,
+} from './chapter-reorg';
+export { lintStructure, type Diagnostic, type LintResult } from './structure-linter';
+export { structureLinterExtension } from './structure-linter-extension';
+export { lintStructure, type Diagnostic, type LintResult } from './structure-linter';
+export { structureLinterExtension } from './structure-linter-extension';
